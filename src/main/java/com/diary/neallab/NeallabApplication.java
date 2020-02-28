f52e41b06064258b1840d0f7e8489f7f4865a781
neallab <neal.k@neallab-2.local>
2020-02-29 01:02:31 +0900

    Initial commit

---
+++ src/main/java/com/diary/neallab/NeallabApplication.java
@@ -0,0 +1,13 @@
+package com.diary.neallab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeallabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeallabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diary/neallab/NeallabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary.neallab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeallabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
