403764609bbf8fac59b91b0b0bc3e8adc8f604fc
neallab <neallab@kakao.com>
2020-02-29 20:46:25 +0900

    이것저것

--- src/main/java/com/diary/neallab/NeallabApplication.java
+++ src/main/java/com/diary/neallab/NeallabApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NeallabApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NeallabApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NeallabApplication.class, args);
+	}
 
 }
